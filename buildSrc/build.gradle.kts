db37cd5257dc7ebf50d59cabb8b3b49e6bb115fe
ankitkumar <lostankit7@gmail.com>
2023-10-23 18:53:21 +0530

    added buildscr from ivy repo

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    `kotlin-dsl`
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation(libs.android.gradle.plugin)
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.detekt.gradle.plugin)
+    implementation(libs.kotlinx.serialization.plugin)
+    implementation(libs.ksp.plugin)
+
+
+    // Make version catalog available in precompiled scripts
+    // https://github.com/gradle/gradle/issues/15383#issuecomment-1567461389
+    implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+}
