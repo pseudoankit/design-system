52595fa84ee85fa88205f53c5673c6cda19de5fc
ankitkumar <lostankit7@gmail.com>
2023-11-18 17:06:35 +0530

    added fontawesome composable

--- app/src/main/java/com/pseudoankit/design_system/MainActivity.kt
+++ app/src/main/java/com/pseudoankit/design_system/MainActivity.kt
@@ -3,14 +3,15 @@ package com.pseudoankit.design_system
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.pseudoankit.bottle.UnifyBottlePreview
 import com.pseudoankit.design_system.ui.theme.DesignsystemTheme
+import com.pseudoankit.icon.FontAwesomeIconDemo
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,29 +19,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             DesignsystemTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState()),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    FontAwesomeIconDemo()
+                    UnifyBottlePreview()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DesignsystemTheme {
-        Greeting("Android")
     }
 }
--- font-awesome-icon/src/main/java/com/pseudoankit/icon/FontAwesomeIcon.kt
+++ font-awesome-icon/src/main/java/com/pseudoankit/icon/FontAwesomeIcon.kt
@@ -1,11 +1,20 @@
 package com.pseudoankit.icon
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -13,7 +22,7 @@ import androidx.compose.ui.unit.sp
 
 /**
  * FaIcon composable to draw Font awesome icons
- * @param faIcon supports solid, regular and brand icons.
+ * @param icon supports solid, regular and brand icons.
  * @param modifier to apply layout changes.
  * @param size Provide the Icon size in DP. Since it's a font to not change Icon size with
  * font scaling of device it ignores device font scaling.
@@ -22,26 +31,32 @@ import androidx.compose.ui.unit.sp
  */
 @Composable
 fun FontAwesomeIcon(
-    faIcon: FaIcon,
+    icon: FaIcon,
     modifier: Modifier = Modifier,
     size: Dp = 24.dp,
     tint: Color = Color.Unspecified,
-    style: TextStyle = TextStyle.Default
+    style: TextStyle = TextStyle.Default,
+    onClick: (() -> Unit)? = null
 ) {
 
     val fontSize = size.scaleIndependentFontSize
 
     val faTextStyle = style.copy(
         color = tint,
-        fontFamily = faIcon.fontFamily,
+        fontFamily = icon.fontFamily,
         fontSize = fontSize
     )
 
-    BasicText(
-        text = faIcon.src,
-        modifier = modifier,
-        style = faTextStyle,
-    )
+    Box(
+        modifier = modifier
+            .clickable(enabled = onClick != null) { onClick?.invoke() },
+        contentAlignment = Alignment.Center
+    ) {
+        BasicText(
+            text = icon.src,
+            style = faTextStyle,
+        )
+    }
 }
 
 private val Dp.scaleIndependentFontSize: TextUnit
@@ -50,3 +65,21 @@ private val Dp.scaleIndependentFontSize: TextUnit
         val materialIconOffset = 3.dp
         return ((this - materialIconOffset).value / scaleFactor).sp
     }
+
+@Preview
+@Composable
+fun FontAwesomeIconDemo() {
+    Column {
+        Text(text = "Font Awesome")
+        FontAwesomeIcon(
+            icon = FaIcon.AddressBook,
+            onClick = {
+
+            },
+            size = 44.dp,
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape),
+        )
+    }
+}
